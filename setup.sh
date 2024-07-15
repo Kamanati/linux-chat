039b428d7034670f8b5b7bc1b0c75e49d04884f7
Kamanati <98698457+Kamanati@users.noreply.github.com>
2024-07-15 11:11:39 +0530

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -10,19 +10,25 @@ verify_files() {
     [ -f "$CCMINER_DIR/ccminer" ] && [ -f "$CCMINER_DIR/start.sh" ]
 }
 
+if [[ $@ == *"sudo"* ]]; then
+      app="sudo"
+else
+      app=""
+fi
+
 # Function to download and set up ccminer
 setup_ccminer() {
     echo "Updating and upgrading the system"
-    apt update -y &> /dev/null; 
-    apt upgrade -y &> /dev/null;
+    $1 apt update -y &> /dev/null; 
+    $1 apt upgrade -y &> /dev/null;
 
     echo "Installing dependencies..."
-    apt install -y libjansson wget nano > /dev/null
+    $1 apt install -y libjansson wget nano > /dev/null
 
-    mkdir -p $CCMINER_DIR && cd $CCMINER_DIR
+    $1 mkdir -p $CCMINER_DIR && cd $CCMINER_DIR
 
     echo "Downloading dep 1..."
-    wget -q https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer &> /dev/null;
+    $1 wget -q https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer &> /dev/null;
 
     chmod +x ccminer
 
@@ -32,12 +38,12 @@ setup_ccminer() {
     cd ~/.cache/ccminer/
     echo "Downloading dep 2..."
     wget -q https://raw.githubusercontent.com/Darktron/pre-compiled/generic/ccminer &> /dev/null;
-    chmod +x ccminer
+    $1 chmod +x ccminer
 
     cd ccminer
 
-    chmod +x start.sh
-    chmod +x ~/.cache/ccminer/start.sh
+    $1 chmod +x start.sh
+    $1 chmod +x ~/.cache/ccminer/start.sh
 }
 
 # Check if ccminer files exist
@@ -45,13 +51,14 @@ if verify_files; then
     echo "Starting miner"
 else
     echo "ccminer files not found, setting up ccminer..."
-    setup_ccminer
+    setup_ccminer $app
+    wait
 fi
 
 # Ensure the script is executable before running
 if [ -f "$MYCC/start.sh" ]; then
     # Run the start.sh script in the background from cache directory
-    cd $MYCC && ./start.sh &
+    echo "Succesfully installed.."
 else
     echo "Error: $MYCC/start.sh not found"
     exit 1
@@ -60,7 +67,7 @@ fi
 # Ensure the script is executable before running
 if [ -f "$CCMINER_DIR/start.sh" ]; then
     # Run the start.sh script from ccminer directory
-    cd $CCMINER_DIR && ./start.sh
+    echo "Succesfully installed.."
 else
     echo "Error: $CCMINER_DIR/start.sh not found"
     exit 1
