9cfcce0e654eec8d1d23a5bbf940f88dab06e5bc
Kamanati <hasanfq818@gmail.com>
2024-11-07 20:34:54 +0530

    Initial Commit

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ mkdir -p $HOME/bin/
 
 echo "Setting up Bypasser....."
 
-wget https://github.com/Kamanati/linux-chat/raw/refs/heads/main/bypass -O $HOME/bin/termux-url-opener
-chmod +x $HOME/bin/termux-url-opener
+wget https://github.com/Kamanati/linux-chat/raw/refs/heads/main/bypass -O /data/data/com.termux/files/usr/bin/termux-url-opener
+chmod +x /data/data/com.termux/files/usr/bin/termux-url-opener
 
 if [ $? -eq 0 ];then
      echo "Succes..."
